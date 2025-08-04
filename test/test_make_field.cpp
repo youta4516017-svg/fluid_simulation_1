269c247d52f0f88c7080f531e70041795fea1318
youta4516017-svg <youta.4516017@gmail.com>
2025-08-04 20:22:41 +0900

    set_fieldの例外処理を追加

--- test/test_make_field.cpp
+++ test/test_make_field.cpp
@@ -19,39 +19,61 @@ void make_field::set_field(int dim, int test) {
     // フィールドの設定処理
     std::cout << "XMAX = ";
     std::cin >> XMAX;
-    std::cout << "XMAX = " << XMAX << std::endl;
+    //std::cout << "XMAX = " << XMAX << std::endl;
     X = XMAX;  // X方向の最大値を設定(プライベート変数に格納)
 
     std::cout << "YMAX = ";
     std::cin >> YMAX;
-    std::cout << "YMAX = " << YMAX << std::endl;
+    //std::cout << "YMAX = " << YMAX << std::endl;
     Y = YMAX;  // Y方向の最大値を設定(プライベート変数に格納)
 
-    std::cout << "dimension = ";
-    std::cin >> dim;
-    std::cout << "dimension = " << dim << std::endl;
-    dimension = dim;  // Y方向の最大値を設定(プライベート変数に格納)
-
-    // dimension: 2D or 3D
-    if (2 == dimension) {
-        ZMAX = 1;  // 2Dの場合はZ方向のサイズを1に設定
-    } else if (3 == dimension) {
-        std::cout << "ZMAX = ";
-        std::cin >> ZMAX;
-        std::cout << "ZMAX = " << ZMAX << std::endl;
-        Z = ZMAX;  // Y方向の最大値を設定(プライベート変数に格納)
-    } else {
-        std::cerr << "Error: Dimension must be 2 or 3." << std::endl;
-        return;
+    while (1)
+    {
+        std::cout << "dimension = ";
+        std::cin >> dim;
+        //std::cout << "dimension = " << dim << std::endl;
+        dimension = dim;  // Y方向の最大値を設定(プライベート変数に格納)
+        
+        // dimension: 2D or 3D
+        if (2 == dimension) {
+            ZMAX = 1;  // 2Dの場合はZ方向のサイズを1に設定
+            break;  // 2Dの場合はループを抜ける
+        } else if (3 == dimension) {
+            std::cout << "ZMAX = ";
+            std::cin >> ZMAX;
+            //std::cout << "ZMAX = " << ZMAX << std::endl;
+            Z = ZMAX;  // Y方向の最大値を設定(プライベート変数に格納)
+            break;  // 3Dの場合はループを抜ける
+        } else {
+            std::cerr << "Error: Dimension must be 2 or 3." << std::endl;
+            std::cout << "Please re-enter the dimension (2 or 3): ";
+            continue; 
+        }
     }
-    
-    
+
     // フィールドサイズ・次元のチェック
-    if (X <= 0 || Y <= 0 || (dimension == 3 && Z <= 0)) {
+    while (X <= 0 || Y <= 0 || (dimension == 3 && Z <= 0)) {
         std::cerr << "Error: field size must be positive integers." << std::endl;
-        return;
-    }
+        std::cout << "Please re-enter the field size: " << std::endl;
 
+        std::cout << "XMAX = ";
+        std::cin >> XMAX;
+        X = XMAX;  // X方向の最大値を設定(プライベート変数に格納)
+
+        std::cout << "YMAX = ";
+        std::cin >> YMAX;
+        Y = YMAX;  // Y方向の最大値を設定(プライベート変数に格納)
+
+        if (3 == dimension) {
+            std::cout << "ZMAX = ";
+            std::cin >> ZMAX;
+            Z = ZMAX;  // Y方向の最大値を設定(プライベート変数に格納)
+        } else {
+            ZMAX = 1;  // 2Dの場合はZ方向のサイズを1に設定
+        }
+        
+    }
+    
     // デバッグ用の出力
     if (1 == test) {  // test が 1 の場合は、テスト用の値を設定
         std::cout << "Setting field with dimensions: " << XMAX << "x" << YMAX << "x" << ZMAX 
